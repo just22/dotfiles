9cb654a5f3d44e4e3774b0c41f7c6493b101f293
just22 <>
2018-10-01 06:52:15 +0000

    Initial commit

---
+++ emacs.d/init.d/init-packages.el
@@ -0,0 +1,29 @@
+; ----------------------------------------------------------------------
+;  $Id$
+;
+;  Emacs packages configuration file
+; ----------------------------------------------------------------------
+
+(provide 'init-packages)
+
+; ----------------------------------------------------------------------
+; Default Emacs package manager
+;
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+; ----------------------------------------------------------------------
+; use-package macro
+;
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
